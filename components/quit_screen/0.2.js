a883c01a3b3b9e6f8d6922ad5b2ded4b8784f32b
Annalise Mariottini <annaliseirene@msn.com>
2017-03-29 13:26:32 -0400

    quitscreen copy prop

--- components/quit_screen/0.2.js
+++ components/quit_screen/0.2.js
@@ -35,26 +35,38 @@ class QuitScreen extends skoash.Screen {
                 {this.renderAssets()}
                 <div className="center">
                     <div className="frame">
-                        <h2>Are you sure you<br/>want to quit?</h2>
-                        <h3>Your game progress will be saved</h3>
-                        <button
-                            className={classNames('quit-button', 'quit-yes',
-                                this.props.quitYesClassName
-                            )}
-                            onClick={this.okay.bind(this)}
-                        />
-                        <button
-                            className={classNames('quit-button', 'quit-no',
-                                this.props.quitNoClassName
-                            )}
-                            onClick={this.cancel.bind(this)}
-                        />
+                        {this.props.copy}
+                        <div className="buttons">
+                            <button
+                                className={classNames('quit-button', 'quit-yes',
+                                    this.props.quitYesClassName
+                                )}
+                                onClick={this.okay.bind(this)}
+                            />
+                            <button
+                                className={classNames('quit-button', 'quit-no',
+                                    this.props.quitNoClassName
+                                )}
+                                onClick={this.cancel.bind(this)}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 }
+
+
+QuitScreen.defaultProps = _.defaults({
+    copy: (
+        <div>
+            <h2>Are you sure you<br/>want to quit?</h2>
+            <h3>Your game progress will be saved</h3>
+        </div>
+    ),
+}, skoash.Screen.defaultProps);
+
 
 export default function (props) {
     return (<QuitScreen
